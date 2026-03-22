1adb3deaf48f832db877ebbc9f9b8bea31ae6a3c
norbertvajko <norbertvajko@yahoo.com>
2026-03-22 21:40:22 +0200

    py origins fixes

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ origins = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
     "https://fuel-prices-romania.onrender.com",
-    "https://your-frontend.netlify.app,"
+    "https://romaniapetrolprices.netlify.app/,"
 ]
 
 app.add_middleware(
